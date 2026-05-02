ab505f0ae1250dbbac0246bd5e96bbc4bd220fdd
aashna08 <aashnagupta008@gmail.com>
2026-05-02 19:59:29 +0530

    Initial commit

---
+++ src/main/java/StringFunctions.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class StringFunctions {
+
+    public void countOccurence(String str)
+    {
+        String st=str.toLowerCase();
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for (int i=0;i<st.length();i++)
+        {
+            if(st.charAt(i)== ' ')
+            {
+                continue;
+            }
+            hm.put(st.charAt(i),hm.getOrDefault(st.charAt(i),0)+1);
+        }
+        System.out.println(hm);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
